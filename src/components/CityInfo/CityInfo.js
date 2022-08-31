3ba83b5049e5f4b34346c3b1c2ee16f27ce86082
Kendall McGree <mcgreekendall@gmail.com>
2022-08-31 17:09:00 -0500

    add files for quality component

--- src/components/CityInfo/CityInfo.js
+++ src/components/CityInfo/CityInfo.js
@@ -1,21 +1,23 @@
 import { useEffect, useState } from "react";
 import { getCities, getCity } from "../../apiCalls";
+import Quality from "../Quality/index";
 
 export const CityInfo = ({ city, cityName, setCity, cities, setCities }) => {
-
   useEffect(() => {
-    let cityId = cities.find(locale => locale.attributes.name === cityName)
+    let cityId = cities.find((locale) => locale.attributes.name === cityName);
 
-    getCity(cityId.id)
-      .then(data => setCity(data.data))
-  },[])
-  
+    getCity(cityId.id).then((data) => setCity(data.data));
+  }, []);
+
   return (
     <>
       <div className="city-info-container">
         <div className="city-name">
-          <p>I'm a freaking CityInfo component!!!!</p>
-          <p>{city.attributes.name}</p>
+          {/* <p>I'm a freaking CityInfo component!!!!</p>
+          <p>{city.attributes.name}</p> */}
+          <h2>{city.attributes.name}</h2>
+          <img src={city.attributes.details.image_mobile_url} />
+          <Quality />
         </div>
       </div>
     </>
---
+++ src/components/Quality/index.js
@@ -0,0 +1,9 @@
+const Quality = () => {
+  return (
+    <>
+      <h2>Hello</h2>
+    </>
+  );
+};
+
+export default Quality;
