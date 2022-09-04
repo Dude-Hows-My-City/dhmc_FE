55ad84869d252d5354a4e839bfe9442abae8a874
Nick Orlov <ialwaysreply@gmail.com>
2022-09-03 23:48:22 -0700

    Add logic to remove selected cities from main list

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,57 +17,74 @@ const App = () => {
   const [city2, setCity2] = useState({});
   const [cityData1, setCityData1] = useState({});
   const [cityData2, setCityData2] = useState({});
-  const [selectedCities, setSelectedCities] = useState([])
-  
+  const [selectedCities, setSelectedCities] = useState([]);
+  const [updatedCities, setUpdatedCities] = useState([]);
 
   useEffect(() => {
-    getCities()
-    .then(data => setCities(data.data))
-  }, [city, filteredNames, city1, city2, cityData2, selectedCities]);
+    if (updatedCities.length < 1) {
+      getCities().then((data) => setCities(data.data));
+    } else {
+      return;
+    }
+  }, [
+    city,
+    filteredNames,
+    city1,
+    city2,
+    cityData2,
+    selectedCities,
+    updatedCities,
+  ]);
 
   const filterNames = (query) => {
-    setFilteredNames(cities.filter((city) => city.attributes.name.toLowerCase().includes(query.toLowerCase())));
+    setFilteredNames(
+      cities.filter((city) =>
+        city.attributes.name.toLowerCase().includes(query.toLowerCase())
+      )
+    );
   };
 
   const findCity = (cityName) => {
-    console.log('findCity in app', cityName)
+    console.log("findCity in app", cityName);
     let foundCity = cities.find((city) => city.attributes.name === cityName);
     setCity(foundCity);
   };
 
   const compareCity = (cityName) => {
-   
-
+    if (updatedCities.length === 0) {
       let foundCity = cities.find((city) => city.attributes.name === cityName);
-      setCity1(foundCity)
-      setSelectedCities([...selectedCities, foundCity])
-      // let cityId = cities.find((locale) => locale.attributes.name === cityName);
-      // getCity(cityId.id).then((data) => setCityData1(data.data));
+      // setCity1(foundCity)
+      setSelectedCities([...selectedCities, foundCity]);
+      let newCities = cities.filter(
+        (city) => city.attributes.name !== cityName
+      );
+      setUpdatedCities(newCities);
+      setCities(newCities)
+    } else {
+      let foundCity = updatedCities.find(
+        (city) => city.attributes.name === cityName
+      );
+      setSelectedCities([...selectedCities, foundCity]);
+      let newCities = updatedCities.filter(
+        (city) => city.attributes.name !== cityName
+      );
+      setUpdatedCities(newCities);
+      setCities(newCities)
 
-    
-  }
-  // const compareCity = (cityName) => {
-  //   if (Object.keys(city1).length === 0) {
+    }
+    // let cityId = cities.find((locale) => locale.attributes.name === cityName);
+    // getCity(cityId.id).then((data) => setCityData1(data.data));
+  };
 
-  //     let foundCity = cities.find((city) => city.attributes.name === cityName);
-  //     setCity1(foundCity)
-  //     setSelectedCities([...selectedCities, foundCity])
-  //     let cityId = cities.find((locale) => locale.attributes.name === cityName);
-  //     getCity(cityId.id).then((data) => setCityData1(data.data));
-  //   } else {
-  //     let foundCity = cities.find((city) => city.attributes.name === cityName);
-  //     setCity2(foundCity)
-  //     let cityId = cities.find((locale) => locale.attributes.name === cityName);
-  //     getCity(cityId.id).then((data) => setCityData2(data.data));
-  //   }
-    
-  // }
-console.log('city in app AR', city)
-console.log('city1 in app AR', city1)
-console.log('city2 in app AR', city2)
-console.log('cityData1 in app AR', cityData1)
-console.log('cityData2 in app AR', cityData2)
-console.log('selectedCities in app AR', selectedCities)
+  console.log("city in app AR", city);
+  console.log("city1 in app AR", city1);
+  console.log("city2 in app AR", city2);
+  console.log("cityData1 in app AR", cityData1);
+  console.log("cityData2 in app AR", cityData2);
+  console.log("selectedCities in app AR", selectedCities);
+  console.log("updatedCities in app AR", updatedCities);
+  console.log(cities);
+
   return (
     <>
       <Header />
@@ -75,30 +92,65 @@ console.log('selectedCities in app AR', selectedCities)
         exact
         path="/info/:city_name"
         render={({ match }) => {
-        return <CityInfo cityName={match.params.city_name} city={city} city1={city1} city2={city2}setCity={setCity} cities={cities} setCities={setCities}/> 
+          return (
+            <CityInfo
+              cityName={match.params.city_name}
+              city={city}
+              city1={city1}
+              city2={city2}
+              setCity={setCity}
+              cities={cities}
+              setCities={setCities}
+            />
+          );
         }}
       />
 
       <Route exact path="/">
         <SearchBar filterNames={filterNames} cities={cities} />
-        {/* { Object.keys(city1).length !== 0 || Object.keys(city2).length !== 0 && */}
-        <SelectedToCompare city1={city1} city2={city2} cities={selectedCities} findCity={findCity} compareCity={compareCity}/>
-{/* } */}
+        <SelectedToCompare
+          city1={city1}
+          city2={city2}
+          cities={selectedCities}
+          findCity={findCity}
+          compareCity={compareCity}
+        />
+
         {filteredNames.length === 0 ? (
-          <CitiesContainer cities={cities} findCity={findCity} compareCity={compareCity} city1={city1} city2={city2} cityData1={cityData1} cityData2={cityData2}/>
+          <CitiesContainer
+            cities={cities}
+            findCity={findCity}
+            compareCity={compareCity}
+            city1={city1}
+            city2={city2}
+            cityData1={cityData1}
+            cityData2={cityData2}
+            selectedCities={selectedCities}
+          />
         ) : (
-          <CitiesContainer filteredNames={filteredNames} findCity={findCity} compareCity={compareCity} city1={city1} city2={city2} cityData1={cityData1} cityData2={cityData2} />
+          <CitiesContainer
+            filteredNames={filteredNames}
+            findCity={findCity}
+            compareCity={compareCity}
+            city1={city1}
+            city2={city2}
+            cityData1={cityData1}
+            cityData2={cityData2}
+            selectedCities={selectedCities}
+          />
         )}
       </Route>
-        
-        {/* { Object.keys(cityData2).length !== 0 && */}
 
-          <Route exact path="/comparison/">
-        <ComparisonPage selectedCities={selectedCities} city={city} city1={city1} city2={city2} cityData1={cityData1} cityData2={cityData2}/>
-
+      <Route exact path="/comparison/">
+        <ComparisonPage
+          selectedCities={selectedCities}
+          city={city}
+          city1={city1}
+          city2={city2}
+          cityData1={cityData1}
+          cityData2={cityData2}
+        />
       </Route>
-       {/* } */}
-
     </>
   );
 };
