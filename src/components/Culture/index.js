736038eccd7f8c5f556071201944a92973fb5c6c
Kendall McGree <mcgreekendall@gmail.com>
2022-09-05 17:27:10 -0500

    Add images for Culture Icons

--- src/components/Culture/index.js
+++ src/components/Culture/index.js
@@ -1,8 +1,13 @@
-const Culture = () => {
+import { StyledCulture } from "../styles/Culture.styled";
+
+const Culture = ({ city }) => {
+  console.log(city.attributes.details.culture);
   return (
-    <>
+    <StyledCulture>
       <h2>Culture</h2>
-    </>
+      <h4>Art Galleries</h4>
+      {/* <img src={}/> */}
+    </StyledCulture>
   );
 };
 
