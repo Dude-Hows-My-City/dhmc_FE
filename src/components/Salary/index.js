261915b1ee55b28f0474378a1b74d0a0f4f46fa7
Kendall McGree <mcgreekendall@gmail.com>
2022-09-05 13:37:41 -0500

    Add initial component layout to Salary with hardcoded data

--- src/components/Salary/index.js
+++ src/components/Salary/index.js
@@ -1,9 +1,23 @@
 import { StyledSalary } from "../styles/Salary.styled";
+import { useState } from "react";
 
-const Salary = () => {
+const Salary = ({ city }) => {
+  const [selectedJob, setSelectedJob] = useState("");
+
+  const handleChange = (event) => {
+    setSelectedJob(event.target.value);
+  };
+
   return (
     <StyledSalary>
-      <h2>Salary</h2>
+      <h2>Salaries</h2>
+      <select value={selectedJob} onChange={handleChange}>
+        <option>--Choose A Job--</option>
+        {/* map over salaries array here */}
+      </select>
+      <h3>Selected Job Title Here</h3>
+      <h4>Average Pay</h4>
+      <p>median value here</p>
     </StyledSalary>
   );
 };
