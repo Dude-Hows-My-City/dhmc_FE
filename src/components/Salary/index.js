88210e49bae79d5f4678a45831649096e140529d
Kendall McGree <mcgreekendall@gmail.com>
2022-09-05 13:48:50 -0500

    Add logic to Salary component to make it dynamically render depending on how many cities have been selected

--- src/components/Salary/index.js
+++ src/components/Salary/index.js
@@ -1,14 +1,14 @@
 import { StyledSalary } from "../styles/Salary.styled";
 import { useState } from "react";
 
-const Salary = ({ city }) => {
+const Salary = ({ city, selectedCities }) => {
   const [selectedJob, setSelectedJob] = useState("");
 
   const handleChange = (event) => {
     setSelectedJob(event.target.value);
   };
-
-  return (
+  //   console.log(selectedCities === undefined);
+  return selectedCities === undefined ? (
     <StyledSalary>
       <h2>Salaries</h2>
       <select value={selectedJob} onChange={handleChange}>
@@ -18,6 +18,10 @@ const Salary = ({ city }) => {
       <h3>Selected Job Title Here</h3>
       <h4>Average Pay</h4>
       <p>median value here</p>
+    </StyledSalary>
+  ) : (
+    <StyledSalary>
+      {/* copy logic from above and add second city's value and city titles */}
     </StyledSalary>
   );
 };
