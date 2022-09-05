5d6295c50ff07ef2fbcf6bb60b92e7c355c7d9ef
Nick Orlov <ialwaysreply@gmail.com>
2022-09-05 01:14:39 -0700

    Add conditional rendering to favorites button

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,20 +1,18 @@
 import { StyledNav } from "../styles/Nav.styled";
 import { useState } from "react";
-import { NavLink } from "react-router-dom";const Nav = () => {
+import { NavLink } from "react-router-dom";
+const Nav = ({favorites}) => {
   return (
     <StyledNav>
-          <NavLink
-          to={`/`}
-          style={{ textDecoration: "none" }}
-          >
-          <h1 data-cy="nav">Dude, How's My City</h1>
-          </NavLink>
-      <NavLink
-          to={`/favorites/`}
-          style={{ textDecoration: "none" }}
-          >
-      <button>FAVORITES</button>
-          </NavLink>
+      <NavLink to={`/`} style={{ textDecoration: "none" }}>
+        <h1 data-cy="nav">Dude, How's My City</h1>
+      </NavLink>
+
+      {favorites.length > 0 &&
+      <NavLink to={`/favorites/`} style={{ textDecoration: "none" }}>
+        <button>FAVORITES</button>
+      </NavLink>
+}
     </StyledNav>
   );
 };
