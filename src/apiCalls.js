ad2e85527872e2d2945a4c2c83c715408faab53c
Nick Orlov <ialwaysreply@gmail.com>
2022-09-04 19:59:46 -0700

    Add postCity to apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -19,3 +19,36 @@ export const getCity = (id) => {
   })
 
 }
+// /api/v1/favorites
+// json body:
+// {
+//     "user_id" : 1,
+//     "city_id" : 1
+// }
+
+// `https://dude-hows-my-city-be.herokuapp.com/api/v1/favorites`
+// export const postCity = (id) => {
+//   let favCity = {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(reservation)
+// }
+// }
+
+
+export const postCity = (id) => {
+  let favCity = {
+    method: 'POST',
+    headers: {
+    "user_id" : 1,
+    "city_id" : id
+    },
+    body: JSON.stringify(id)
+  };
+
+  fetch("https://dude-hows-my-city-be.herokuapp.com/api/v1/favorites", favCity)
+    .then(response => response.json())
+    .catch(error => console.log('error', error));
+}
