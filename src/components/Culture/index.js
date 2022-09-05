7c5703ef04d5693030eb200d1bad0d50e7fa9d17
Kendall McGree <mcgreekendall@gmail.com>
2022-09-05 17:21:37 -0500

    Add all files for Culture component

---
+++ src/components/Culture/index.js
@@ -0,0 +1,9 @@
+const Culture = () => {
+  return (
+    <>
+      <h2>Culture</h2>
+    </>
+  );
+};
+
+export default Culture;
---
+++ src/components/styles/Culture.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledCulture = styled.section`
+  border: solid 3px red;
+  border-radius: 1em;
+  margin-left: 5%;
+  width: 90%;
+`;
