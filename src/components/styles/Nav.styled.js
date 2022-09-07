d01023a8a9d8568c5ea208e7cb61691f934eb487
Kendall McGree <mcgreekendall@gmail.com>
2022-09-07 12:14:11 -0500

    Update Header styling

--- src/components/styles/Nav.styled.js
+++ src/components/styles/Nav.styled.js
@@ -1,5 +1,18 @@
 import styled from "styled-components";
 
 export const StyledNav = styled.section`
+  background-color: #d8dad7;
+  border-bottom-left-radius: 1em;
+  border-bottom-right-radius: 1em;
+  height: 8vh;
   text-align: center;
+
+  h1 {
+    cursor: pointer;
+    font-size: 2em;
+    font-style: italic;
+    font-weight: 700;
+    color: limegreen;
+    padding-top: 0.5em;
+  }
 `;
