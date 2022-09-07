efc9828f14291da4bfa679fca9e345c9ed14f52b
Kendall McGree <mcgreekendall@gmail.com>
2022-09-07 09:38:39 -0500

    Add error message in the event the user deletes all favorites

---
+++ src/components/styles/Favorites.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledFavorites = styled.div`
+  h1 {
+    color: red;
+    text-align: center;
+    margin-top: 5em;
+  }
+
+  img {
+    height: 40vh;
+    margin-left: 2em;
+    width: 80vw;
+  }
+`;
