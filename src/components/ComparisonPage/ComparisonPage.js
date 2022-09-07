c5a363f631dcb4bd2ddaca683f74a626f8eafe25
Kendall McGree <mcgreekendall@gmail.com>
2022-09-07 09:22:21 -0500

    Add error handling for both Quality components

--- src/components/ComparisonPage/ComparisonPage.js
+++ src/components/ComparisonPage/ComparisonPage.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import Quality from "../Quality/QualityIndex";
 import Housing from "../Housing/HousingIndex";
 import { StyledComparisonPage } from "../styles/ComparisonPage.styled";
@@ -19,10 +18,8 @@ export const ComparisonPage = ({ selectedCities }) => {
               src={selectedCities[0].attributes.details.image_mobile_url}
             />
             <Quality city={selectedCities[0]} />
-            {/* <Housing city={selectedCities[0]} /> */}
           </div>
         </div>
-
         <div data-cy="city-info-container" className="city-info-container">
           <div data-cy="city-info-name-wrapper" className="city-name">
             <h2 data-cy="city-info-name">
@@ -33,7 +30,6 @@ export const ComparisonPage = ({ selectedCities }) => {
               src={selectedCities[1].attributes.details.image_mobile_url}
             />
             <Quality city={selectedCities[1]} />
-            {/* <Housing city={selectedCities[1]} /> */}
           </div>
         </div>
       </section>
--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -5,15 +5,19 @@ import { StyledProgress } from "../styles/Progress.styled";
 const Progress = ({ score, city, name }) => {
   return (
     <StyledProgress>
-      <div data-cy="progess-bar" className="progress-bar">
-        <p>{name}</p>
-        <ProgressBar
-          now={score}
-          min={0}
-          max={10}
-          label={`${Math.round(score)}/10`}
-        />
-      </div>
+      {score ? (
+        <div data-cy="progess-bar" className="progress-bar">
+          <p>{name}</p>
+          <ProgressBar
+            now={score}
+            min={0}
+            max={10}
+            label={`${Math.round(score)}/10`}
+          />
+        </div>
+      ) : (
+        <p></p>
+      )}
     </StyledProgress>
   );
 };
