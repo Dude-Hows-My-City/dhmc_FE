13e4861540a23390dbc94d7e9219e1535a81ecff
Kendall McGree <mcgreekendall@gmail.com>
2022-09-07 09:01:12 -0500

    Add error handling to Housing component for CityInfo page

--- src/components/Housing/HousingIndex.js
+++ src/components/Housing/HousingIndex.js
@@ -8,21 +8,33 @@ const Housing = ({ city, selectedCities }) => {
       <div data-cy="apartments-container" className="apartments">
         <div data-cy="apt-small" className="small">
           <h3 data-cy="apt-small-head">Small Apartment</h3>
-          <p data-cy="apt-small-num">
-            {city.attributes.details.housing.small_apartment}
-          </p>
+          {city.attributes.details.housing.small_apartment ? (
+            <p data-cy="apt-small-num">
+              {city.attributes.details.housing.small_apartment}
+            </p>
+          ) : (
+            <p>N/A</p>
+          )}
         </div>
         <div data-cy="apt-medium" className="medium">
           <h3 data-cy="apt-medium-head">Medium Apartment</h3>
-          <p data-cy="apt-medium-num">
-            {city.attributes.details.housing.medium_apartment}
-          </p>
+          {city.attributes.details.housing.medium_apartment ? (
+            <p data-cy="apt-medium-num">
+              {city.attributes.details.housing.medium_apartment}
+            </p>
+          ) : (
+            <p>N/A</p>
+          )}
         </div>
         <div data-cy="apt-large" className="large">
           <h3 data-cy="apt-large-head">Large Apartment</h3>
-          <p data-cy="apt-large-num">
-            {city.attributes.details.housing.large_apartment}
-          </p>
+          {city.attributes.details.housing.large_apartment ? (
+            <p data-cy="apt-large-num">
+              {city.attributes.details.housing.large_apartment}
+            </p>
+          ) : (
+            <p>N/A</p>
+          )}
         </div>
       </div>
     </StyledHousing>
